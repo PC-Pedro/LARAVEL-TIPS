fa5194817c996ffc12b5dbbbb73f3681ec35ebd8
Pedro Eduardo Serafim Roque <pedro.roque@ngpromotora.com.br>
2024-08-26 09:40:34 -0300

    # main# Aprendendo conceitos do Laravel

---
+++ ep01/app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller{
+
+    public function listUser(){
+        echo "<h1>Listagem de Usuário</h1>";
+        echo "Teste";  
+    }
+}
+
